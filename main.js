0e85e8fb70b431b1462f1596e87a89285be55214
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-08-08 18:52:23 -0500

    primer avance

---
+++ main.js
@@ -0,0 +1,14 @@
+const accordions = document.getElementsByClassName('accordion');
+var i;
+
+for (i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+    });
+}
